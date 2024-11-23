ebad58d2095619d903d2e1f0a5a3967bd3829e7d
sauravmax <sauravmi.7297@gmail.com>
2024-11-23 11:59:28 +0530

    added testfor negative numbers

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -10,6 +10,18 @@ describe("add calculator", () => {
   it("two number should return sum", () => {
     expect(add("2,3")).toBe(5);
   });
+
+  it("any numbers of string should return sum", () => {
+    expect(add("1,2,3,3,5,6")).toBe(20);
+  });
+
+  it("negative numbers not allowed", () => {
+    expect(add("1,2,-3, -100")).toBe("negative numbers not allowed -3, -100");
+    ;
+  });
+
+  
+
 });
 
 const add = (numbers) => {
@@ -17,7 +29,10 @@ const add = (numbers) => {
   if (numbers == "") return 0;
   else {
     const numArray = numbers.split(",");
-
+    const negativeArray = numArray.filter(e => e < 0);
+    if(negativeArray.length > 0){
+        return `negative numbers not allowed ${negativeArray.join(',')}`
+    }
     numArray.forEach((n) => {
       sum = sum + parseInt(n);
     });
