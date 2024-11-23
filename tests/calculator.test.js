020c022ec0ee97bff3250d2bf1fb391e41905ede
sauravmax <sauravmi.7297@gmail.com>
2024-11-23 11:25:40 +0530

    added test for single number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,8 +2,15 @@ describe('add calculator', () => {
     it('empty string should return 0', () => {
         expect(add('')).toBe(0);
     })
+
+    it('Single number should return that no it self', () => {
+        expect(add('2')).toBe(2);
+    })
+
+    
 })
 
 const add = (numbers) => {
     if(numbers == '') return 0
+    if(numbers.length == 1) return parseInt(numbers)
 }
