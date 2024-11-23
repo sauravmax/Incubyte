879e97fb2b3eaa9338e2b9b09a7d15c154d1b15f
sauravmax <sauravmi.7297@gmail.com>
2024-11-23 11:32:07 +0530

    added test case for 2 number addition

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,16 +1,27 @@
-describe('add calculator', () => {
-    it('empty string should return 0', () => {
-        expect(add('')).toBe(0);
-    })
+describe("add calculator", () => {
+  it("empty string should return 0", () => {
+    expect(add("")).toBe(0);
+  });
 
-    it('Single number should return that no it self', () => {
-        expect(add('2')).toBe(2);
-    })
+  it("Single number should return that no it self", () => {
+    expect(add("2")).toBe(2);
+  });
 
-    
-})
+  it("two number should return sum", () => {
+    expect(add("2,3")).toBe(5);
+  });
+});
 
 const add = (numbers) => {
-    if(numbers == '') return 0
-    if(numbers.length == 1) return parseInt(numbers)
-}
+  let sum = 0;
+  if (numbers == "") return 0;
+  if (numbers.length == 1) return parseInt(numbers);
+  else {
+    const numArray = numbers.split(",");
+
+    numArray.forEach((n) => {
+      sum = sum + parseInt(n);
+    });
+    return sum;
+  }
+};
